5b4d3b8752f05fb7cb3c94a0e11103e3097d262f
Michael J Gorman <michael@michaeljgorman.com>
2014-09-06 12:37:41 -0700

    Ch1 files

---
+++ ch1/zipCracker.py
@@ -0,0 +1,31 @@
+import zipfile
+import optparse
+from threading import Thread
+
+def extractFile(zFile, password):
+    try: 
+        zFile.extractall(pwd=password)
+        print '[+] Found Password ' + password + '\n'
+    except:
+       pass
+
+def main():
+    parser = optparse.OptionParser("usage%prog -f <zipfile> -d <dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+    (option, args) = parser.parse_args()
+    if (option.zname == None) | (option.dname == None):
+        print parser.usage
+        exit(0)
+    else:
+        zname = option.zname
+        dname = option.dname
+    zFile = zipfile.ZipFile(zname) 
+    passFile = open(dname) 
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target=extractFile, args=(zFile, password))
+        t.start()
+
+if __name__ == '__main__':
+    main()
